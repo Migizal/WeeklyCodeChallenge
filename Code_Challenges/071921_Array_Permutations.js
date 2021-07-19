eb32dfbc6bed27d0b20140759b68beed5f6cbd12
Miguel McCrary <migmccrary@gmail.com>
2021-07-19 15:39:03 -0500

    Update 071921_Array_Permutations.js

--- Code_Challenges/071921_Array_Permutations.js
+++ Code_Challenges/071921_Array_Permutations.js
@@ -1,3 +1,17 @@
+/*
+Given array of distinct integers, print all permutations of the array.
+For example :
+array : [10, 20, 30]
+
+Permutations are :
+
+[10, 20, 30]
+[10, 30, 20]
+[20, 10, 30]
+[20, 30, 10]
+[30, 10, 20]
+[30, 20, 10] 
+*/
 function permute(permutation) {
     var length = permutation.length,
         result = [permutation.slice()],
