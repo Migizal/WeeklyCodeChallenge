c39b9314cf9865e9bed8674efa3fbfa39fc327b9
Miguel McCrary <migmccrary@gmail.com>
2021-07-26 12:35:51 -0500

    Add files via upload

---
+++ Code_Challenges/Contiguous-Subarray.js
@@ -0,0 +1,33 @@
+let arr = [14,12,70,15,99,65,21,90];
+let X = 85;
+let newTotal, i, j;
+let sumElements = [];
+//Sum found between index 1 and 3
+//Elements are 12, 17 and 15
+
+for(i = 0; i < arr.length; i++){
+    newTotal = arr[i];
+    sumElements = [arr[i]];
+    for(j = i + 1; j < arr.length; j++){
+        if(newTotal == X){
+            if(i == (j-1)){
+                console.log("Sum found at index " + i);
+            }else{
+                console.log("Sum found between index " + i + " and " + (j-1));
+            }
+            console.log("Elements are "+ sumElements);
+            return newTotal;
+
+        }
+        if(newTotal < X){
+            newTotal += arr[j];
+            sumElements.push(arr[j]);
+        }
+        if(newTotal > X){
+            break;
+        }
+       
+    }
+}
+console.log("No Subarray totals the required value");
+
