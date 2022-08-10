ebef64fba530293b3c7c19205e767be86dc56a95
Miguel McCrary <migmccrary@gmail.com>
2022-08-10 15:14:00 -0500

    New weekly challenges

---
+++ Perimeter.js
@@ -0,0 +1,8 @@
+function perimeter(l, num){
+
+    return (l == "s") ? 4*num : 6.28*num;
+
+}
+console.log(perimeter("s", 7));
+console.log(perimeter("c", 4));
+console.log(perimeter("c", 9));
---
+++ RightShift.js
@@ -0,0 +1,7 @@
+function RightShift(x, y) 
+{
+    return solution = Math.floor(x/(Math.pow(2, y)));
+}
+console.log(RightShift(80, 3));
+console.log(RightShift(-24, 2));
+console.log(RightShift(-5, 1));
