1e0956ae5bc99f7513f519384851222217476ae1
Miguel McCrary <migmccrary@gmail.com>
2022-08-19 16:22:25 -0500

    Added description of the challenge for palindrome

--- Code_Challenges/Palindrome.js
+++ Code_Challenges/Palindrome.js
@@ -1,3 +1,21 @@
+/*
+Weekly Coding Challenge: Palindrome Given a word, create a function that returns whether or not it's possible to create a palindrome by rearranging the letters in the word.
+
+Examples 
+
+isPalindromePossible("rearcac") ➞ true
+// You can make "racecar"
+isPalindromePossible("suhbeusheff") ➞ true
+// You can make "sfuehbheufs" (not a real word but still a palindrome)
+isPalindromePossible("palindrome") ➞ false
+// It's impossible
+
+
+Notes
+
+Trivially, words that are already palindromes return true.	
+Words are given in all lowercase.
+*/
 let NUM_OF_CHARS = 256;
 
 function Palindrome(string){
@@ -21,4 +39,3 @@ function Palindrome(string){
 console.log('Is \'rearcac\' a Palindrome?: '+ Palindrome('rearcac'));
 console.log('Is \'suhbeusheff\' a Palindrome?: '+ Palindrome('suhbeusheff'));
 console.log('Is \'palindrome\' a Palindrome?: '+ Palindrome('palindrome'));
-
